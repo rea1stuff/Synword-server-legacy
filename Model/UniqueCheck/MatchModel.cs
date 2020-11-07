0ad75e92b7428e28f288125eea39859b42863a2a
DarkArTes2 <50269308+DarkArTes2@users.noreply.github.com>
2020-11-07 20:04:22 +0300

    Little changes for Json serialize

--- Model/UniqueCheck/MatchModel.cs
+++ Model/UniqueCheck/MatchModel.cs
@@ -1,6 +1,10 @@
+using Newtonsoft.Json;
+
 namespace SynWord_Server_CSharp.Model.UniqueCheck {
     public class Match {
+        [JsonProperty("url")]
         public string Url { get; set; }
+        [JsonProperty("percent")]
         public float Percent { get; set; }
     }
 }
--- Model/UniqueCheck/UniqueCheckResponseModel.cs
+++ Model/UniqueCheck/UniqueCheckResponseModel.cs
@@ -1,6 +1,10 @@
+using Newtonsoft.Json;
+
 namespace SynWord_Server_CSharp.Model.UniqueCheck {
     public class UniqueCheckResponseModel {
+        [JsonProperty("percent")]
         public float Percent { get; set; }
+        [JsonProperty("matches")]
         public Match[] Matches { get; set; }
     }
 }
