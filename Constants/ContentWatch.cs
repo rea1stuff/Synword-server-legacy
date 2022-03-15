907d3a500407539b6fcd2a19d277b4526c63bcd4
rea1stuff <40441464+rea1stuff@users.noreply.github.com>
2022-03-15 20:59:37 +0300

    remove confidential data

---
+++ Constants/ContentWatch.cs
@@ -0,0 +1,7 @@
+namespace SynWord_Server_CSharp.Constants
+{
+    public static class ContentWatch
+    {
+        public const string ApiUrl = "https://content-watch.ru/public/api/";
+    }
+}
---
+++ UniqueCheck/UniqueCheckFromContentWatchApi.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SynWord_Server_CSharp.Constants;
+using SynWord_Server_CSharp.Model.UniqueCheck;
+
+namespace SynWord_Server_CSharp.UniqueCheck {
+    public class UniqueCheckFromContentWatchApi {
+        private readonly string _apiKey = ConfigurationManager.AppSettings["contentWatchApiKey"];
+
+        public async Task<UniqueCheckResponseModel> PostReqest(string text) {
+            HttpClient httpClient = new HttpClient();
+
+            Dictionary<string, string> values = new Dictionary<string, string> {
+                    { "key", _apiKey },
+                    { "text", text },
+                    { "test", "1" }
+            };
+
+            FormUrlEncodedContent content = new FormUrlEncodedContent(values);
+
+            HttpResponseMessage response = await httpClient.PostAsync(ContentWatch.ApiUrl, content);
+
+            if (!response.IsSuccessStatusCode) {
+                throw new Exception("Unique check error");
+            }
+
+            string responseString = await response.Content.ReadAsStringAsync();
+            ContentWatchApiModel contentWatchModel = JsonConvert
+                .DeserializeObject<ContentWatchApiModel>(responseString);
+
+            if (contentWatchModel.Error != string.Empty) {
+                throw new Exception(contentWatchModel.Error);
+            }
+
+            UniqueCheckResponseModel uniqueCheckResponse = new UniqueCheckResponseModel()
+            {
+                Percent = contentWatchModel.Percent,
+                Matches = contentWatchModel.Matches
+            };
+
+            return uniqueCheckResponse;
+        }
+    }
+}
